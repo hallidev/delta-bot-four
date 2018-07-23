957166e574cbda536e404495d0305a26cc7aa082
Jon Halliday <Jon.Halliday@insurity.com>
2018-07-23 09:38:47 -0400

    Failure to update Deltaboards will no longer cause the award delta process to fail

--- DeltaBotFour.Infrastructure/Implementation/DeltaboardEditor.cs
+++ DeltaBotFour.Infrastructure/Implementation/DeltaboardEditor.cs
@@ -7,6 +7,7 @@ using DeltaBotFour.Infrastructure.Interface;
 using DeltaBotFour.Models;
 using DeltaBotFour.Persistence.Interface;
 using DeltaBotFour.Reddit.Interface;
+using DeltaBotFour.Shared.Logging;
 
 namespace DeltaBotFour.Infrastructure.Implementation
 {
@@ -18,6 +19,7 @@ namespace DeltaBotFour.Infrastructure.Implementation
         private readonly AppConfiguration _appConfiguration;
         private readonly IDB4Repository _db4Repository;
         private readonly ISubredditService _subredditService;
+        private readonly ILogger _logger;
         private readonly string _deltaboardsTemplate;
         private readonly string _deltaboardTemplate;
         private readonly string _deltaboardRowTemplate;
@@ -25,11 +27,13 @@ namespace DeltaBotFour.Infrastructure.Implementation
 
         public DeltaboardEditor(AppConfiguration appConfiguration, 
             IDB4Repository db4Repository,
-            ISubredditService subredditService)
+            ISubredditService subredditService,
+            ILogger logger)
         {
             _appConfiguration = appConfiguration;
             _db4Repository = db4Repository;
             _subredditService = subredditService;
+            _logger = logger;
 
             _deltaboardsTemplate = File.ReadAllText(appConfiguration.TemplateFiles.DeltaboardsTemplateFile);
             _deltaboardTemplate = File.ReadAllText(appConfiguration.TemplateFiles.DeltaboardTemplateFile);
@@ -62,44 +66,52 @@ namespace DeltaBotFour.Infrastructure.Implementation
 
         private void buildAndUpdateDeltaboards()
         {
-            // Get the updated deltaboards
-            var deltaboards = getDeltaboards();
-
-            // Build the actual string content
-            string updatedDeltaboards = buildDeltaboardsContent(deltaboards);
-
-            // Update the wiki page
-            _subredditService.EditWikiPage(_appConfiguration.WikiUrlDeltaboards, updatedDeltaboards, UpdateDeltaboarsdReason);
-
-            // Get sidebar
-            string sidebar = _subredditService.GetSidebar();
-
-            var sidebarDeltaboardMatch = _appConfiguration.DeltaboardSidebarRegex.Match(sidebar);
-
-            // Build just the monthly
-            string monthlyDeltaboardContent =
-                buildSidebarDeltaboard(deltaboards.First(db => db.DeltaboardType == DeltaboardType.Monthly));
-
-            // Note: the second group is the content we're interested in
-            if (sidebarDeltaboardMatch.Groups.Count == 2)
+            try
             {
-                string currentSidebarDeltaboard = sidebarDeltaboardMatch.Groups[1].Value;
-                string updatedSidebarDeltaboard =
-                    $"\r\n{monthlyDeltaboardContent}";
+                // Get the updated deltaboards
+                var deltaboards = getDeltaboards();
 
-                // Update sidebar with new content
-                string updatedSidebar = sidebar.Replace(currentSidebarDeltaboard, updatedSidebarDeltaboard);
+                // Build the actual string content
+                string updatedDeltaboards = buildDeltaboardsContent(deltaboards);
 
-                _subredditService.UpdateSidebar(updatedSidebar);
+                // Update the wiki page
+                _subredditService.EditWikiPage(_appConfiguration.WikiUrlDeltaboards, updatedDeltaboards, UpdateDeltaboarsdReason);
+
+                // Get sidebar
+                string sidebar = _subredditService.GetSidebar();
+
+                var sidebarDeltaboardMatch = _appConfiguration.DeltaboardSidebarRegex.Match(sidebar);
+
+                // Build just the monthly
+                string monthlyDeltaboardContent =
+                    buildSidebarDeltaboard(deltaboards.First(db => db.DeltaboardType == DeltaboardType.Monthly));
+
+                // Note: the second group is the content we're interested in
+                if (sidebarDeltaboardMatch.Groups.Count == 2)
+                {
+                    string currentSidebarDeltaboard = sidebarDeltaboardMatch.Groups[1].Value;
+                    string updatedSidebarDeltaboard =
+                        $"\r\n{monthlyDeltaboardContent}";
+
+                    // Update sidebar with new content
+                    string updatedSidebar = sidebar.Replace(currentSidebarDeltaboard, updatedSidebarDeltaboard);
+
+                    _subredditService.UpdateSidebar(updatedSidebar);
+                }
+
+                // Update monthly deltaboard sidebar widget for reddit redesigned site
+                // HACK: The sidebar widget doesn't need the header, so get rid of it
+                string monthlyNoHeader = monthlyDeltaboardContent
+                    .Replace("###### Monthly Deltaboard", string.Empty)
+                    .TrimStart(Environment.NewLine.ToCharArray());
+
+                _subredditService.UpdateSidebarWidget(_appConfiguration.DeltaboardSidebarWidgetName, monthlyNoHeader);
             }
-
-            // Update monthly deltaboard sidebar widget for reddit redesigned site
-            // HACK: The sidebar widget doesn't need the header, so get rid of it
-            string monthlyNoHeader = monthlyDeltaboardContent
-                .Replace("###### Monthly Deltaboard", string.Empty)
-                .TrimStart(Environment.NewLine.ToCharArray());
-
-            _subredditService.UpdateSidebarWidget(_appConfiguration.DeltaboardSidebarWidgetName, monthlyNoHeader);
+            catch (Exception ex)
+            {
+                // Shouldn't crash if Deltaboards can't be updated. Swallow / log exception
+                _logger.Error(ex, "Error while updating Deltaboards.");
+            }
         }
 
         private string buildDeltaboardsContent(List<Deltaboard> deltaboards)
